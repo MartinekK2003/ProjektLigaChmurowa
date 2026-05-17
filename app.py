42f81ca1a5ef744908c37b0968cb91f327a20031
Martin Kowalcze <241203@stud.usz.edu.pl>
2026-05-17 16:09:35 +0200

    Dodanie logiki bazy SQLite i konfiguracji pod Azure

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Działa Flask na Azure!"
+# Inteligentne wykrywanie środowiska
+if 'WEBSITE_SITE_NAME' in os.environ:
+    # Jesteśmy w chmurze Azure (Środowisko Produkcyjne)
+    # Zapisujemy plik bazy w specjalnym folderze /home, który zaraz utrwalimy
+    db_path = '/home/site/liga_chmurowa.db'
+else:
+    # Jesteśmy na komputerze dewelopera (Lokalnie)
+    db_path = 'liga_chmurowa.db'
 
-if __name__ == "__main__":
-    app.run()
+# Konfiguracja połączenia SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Tutaj zdefiniujcie swoje klasy z diagramu, np:
+class League(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+# Utworzenie bazy (wywoła się automatycznie przy starcie)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
